65b777f215c577ca5ecedd15e3f80b1672f10057
p55d2k <kai@xcidic.com>
2024-04-03 20:36:42 +0800

    bugfix: broken link on footer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -61,7 +61,7 @@ const config: Config = {
           items: [
             {
               label: "Docs",
-              to: "/docs/intro",
+              to: "/docs/about",
             },
           ],
         },
